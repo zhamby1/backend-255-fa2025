2626ab8326df2f9b0803a830b190c6e88ecd3865
Zach Hamby <zhamby1@ivytech.edu>
2025-12-03 10:59:46 -0500

    added routes for delete edit add

--- app.js
+++ app.js
@@ -26,6 +26,60 @@ router.get("/songs", async(req,res) =>{
     }
 })
 
+//if i want to grab a single song i can use the query parameters to do so....
+//i am going to use what we call a router parameter to say that 'hey backend, this part of the url can change and will be grabbed from the front end'
+router.get("/songs/:id", async(req,res) =>{
+    try{
+        const song = await Song.findById(req.params.id)
+        res.json(song)
+    }
+    catch(err){
+        res.status(400).send(err)
+
+    }
+})
+
+//update or edit a song we need an existing record and update it based on an id
+router.put("/songs/:id", async(req,res) =>{
+    //first we need to find and update the song from the front end
+    //we can use this by passing ids and then putting the information in the form and then changing it
+    //so we will grab the form data and then take that form data and update it
+    try{
+        const song = req.body
+        await Song.updateOne({_id: req.params.id}, song)
+        res.sendStatus(204)
+    }
+    catch(err){
+        res.status(400).send(err)
+
+    }
+})
+
+//delete a song
+//update or edit a song we need an existing record and update it based on an id
+router.delete("/delete/:id", async(req,res) =>{
+    //first we need to find and update the song from the front end
+    //we can use this by passing ids and then putting the information in the form and then changing it
+    //so we will grab the form data and then take that form data and update it
+    try{
+       
+        const result = await Song.deleteOne({_id: req.params.id})
+        if (result.deletedCount === 0){
+            res.sendStatus(404)
+        }
+        else{
+            res.sendStatus(204)
+        }
+    }
+    catch(err){
+        res.status(400).send(err)
+
+    }
+})
+
+
+
+
 //to add a song we are going to eventually get it from a form...
 //for now we will use thunderclient,flashpost,  or postman to test the ability to make a song
 //when we send a song to the backend, it will be in the form of a post request
